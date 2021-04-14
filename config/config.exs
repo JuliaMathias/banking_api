ec2687259c386c159dd555072f0076a4159b50d0
JuliaMathias <juliamss@gmail.com>
2021-04-14 15:57:56 -0300

    Add configuration so that migrations are automatically UUID

--- config/config.exs
+++ config/config.exs
@@ -18,6 +18,10 @@ config :banking_api, BankingApiWeb.Endpoint,
   pubsub_server: BankingApi.PubSub,
   live_view: [signing_salt: "1ewXIMc/"]
 
+config :banking_api, BankingApi.Repo,
+  migration_primary_key: [type: :binary_id],
+  migration_foreign_key: [type: :binary_id]
+
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
