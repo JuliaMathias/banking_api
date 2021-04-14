62e6bece51c2b59503c28183a64262c541febf3a
JuliaMathias <juliamss@gmail.com>
2021-04-14 15:24:53 -0300

    Create User Table migration

---
+++ priv/repo/migrations/20210414182242_create_user_table.exs
@@ -0,0 +1,18 @@
+defmodule Rocketpay.Repo.Migrations.CreateUserTable do
+  @moduledoc """
+  Creates User Table
+  """
+  use Ecto.Migration
+
+  def change do
+    create table :users do
+      add :name, :string
+      add :email, :string
+      add :password_hash, :string
+
+      timestamps()
+    end
+
+    create unique_index(:users, [:email])
+  end
+end
