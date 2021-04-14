e9fb5e322dfbf126ea5af321813b49d01aa74a1a
JuliaMathias <juliamss@gmail.com>
2021-04-14 17:27:37 -0300

    Add User schema

---
+++ lib/banking_api/users/schemas/user.ex
@@ -0,0 +1,37 @@
+defmodule BankingApi.Users.Schemas.User do
+  @moduledoc """
+  Add user config
+  """
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @required_params [:name, :email, :password]
+
+  schema "users" do
+    field :name, :string
+    field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:password, min: 8)
+    |> validate_format(:email, ~r/^[A-Za-z0-9\._%+\-+']+@[A-Za-z0-9\.\-]+\.[A-Za-z]{2,4}$/)
+    |> unique_constraint([:email])
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Bcrypt.add_hash(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
+end
